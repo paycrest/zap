a1ba8be3290654a6b0a98ecaa0167f65a9217c60
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2024-06-24 15:58:27 +0100

    feat: add create wallet functionality
    This commit adds the functionality to create a wallet by connecting with the Coinbase Wallet SDK.

--- app/components/WalletButtons.tsx
+++ app/components/WalletButtons.tsx
@@ -1,12 +1,30 @@
+import { useCallback } from "react";
+import { useConnect } from "wagmi";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+
 import { secondaryBtnClasses } from "./Styles";
 
 export const WalletButtons = () => {
+  const { connectors, connect, data } = useConnect();
+
+  const createWallet = useCallback(() => {
+    const coinbaseWalletConnector = connectors.find(
+      (connector) => connector.id === "coinbaseWalletSDK",
+    );
+    if (coinbaseWalletConnector) {
+      connect({ connector: coinbaseWalletConnector });
+    }
+  }, [connectors, connect]);
+
   return (
     <>
       <ConnectButton accountStatus={"avatar"} chainStatus={"none"} />
 
-      <button type="button" className={secondaryBtnClasses}>
+      <button
+        type="button"
+        className={secondaryBtnClasses}
+        onClick={createWallet}
+      >
         Create Wallet
       </button>
     </>
