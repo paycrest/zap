236a91193b3fe1bbfc62c697e0ef3ecf869f37b4
Chibuotu Amadi <chibuotu@gmail.com>
2024-06-30 02:28:26 +0100

    feat: fix condition for 2-step wallet permissions notice

--- app/pages/TransactionPreview.tsx
+++ app/pages/TransactionPreview.tsx
@@ -452,14 +452,14 @@ export const TransactionPreview = ({
       <hr className="w-full border-dashed border-gray-200 dark:border-white/10" />
 
       {/* Confirm and Approve */}
-      {(gatewayAllowance >= amount || smartTokenBalance == amount) && (
+      {(gatewayAllowance < amount || smartTokenBalance == amount) && (
         <p className="text-gray-500 dark:text-white/50">
           To confirm order, you&apos;ll be required to approve these two
           permissions from your wallet
         </p>
       )}
 
-      {(gatewayAllowance >= amount || smartTokenBalance == amount) && (
+      {(gatewayAllowance < amount || smartTokenBalance == amount) && (
         <div className="flex items-center justify-between text-gray-500 dark:text-white/50">
           <p>
             {/* replace 1 with 2 when the approve state is set to complete */}
