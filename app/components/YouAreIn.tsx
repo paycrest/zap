db1a3ca067420f5d472de47c568b7bfee70133ce
Jeremy <aworetanjeremiah@gmail.com>
2024-09-24 23:30:23 +0100

    feat: add animated confetti to waitlist success page
    Also ignore .env file

--- app/components/YouAreIn.tsx
+++ app/components/YouAreIn.tsx
@@ -1,17 +1,22 @@
-import { ConfettiIcon } from "./ImageAssets";
-import { buttonStyles } from "./WaitlistForm";
-import { AnimatedComponent, fadeInOut, slideInOut } from "./AnimatedComponents";
+import { buttonStyles } from './WaitlistForm';
+import { AnimatedComponent, fadeInOut, slideInOut } from './AnimatedComponents';
+import Lottie from 'lottie-react';
+import Confetti from '../json/confetti.json';
 
 export const YouAreIn = ({ onDone }: { onDone: () => void }) => {
   return (
     <>
-      <AnimatedComponent variant={fadeInOut} delay={0.4}>
-        <ConfettiIcon />
+      <AnimatedComponent
+        variant={fadeInOut}
+        delay={0.4}
+        className='w-full max-w-40'
+      >
+        <Lottie animationData={Confetti} alt='confetti' />
       </AnimatedComponent>
 
       <AnimatedComponent
         variant={slideInOut}
-        className="text-neutral-900 dark:text-white text-3xl font-semibold"
+        className='text-neutral-900 dark:text-white text-3xl font-semibold'
       >
         You are in
       </AnimatedComponent>
@@ -19,14 +24,14 @@ export const YouAreIn = ({ onDone }: { onDone: () => void }) => {
       <AnimatedComponent
         variant={fadeInOut}
         delay={0.4}
-        className="text-neutral-900 dark:text-white/80 font-light"
+        className='text-neutral-900 dark:text-white/80 font-light'
       >
         Hey champ, your email has been received. We will notify your inbox when
         we launch
       </AnimatedComponent>
 
       <AnimatedComponent variant={fadeInOut} delay={0.6}>
-        <button type="button" className={buttonStyles} onClick={onDone}>
+        <button type='button' className={buttonStyles} onClick={onDone}>
           Done
         </button>
       </AnimatedComponent>
