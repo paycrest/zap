0f4d77329d8f413a5931ef7eb72a4a1f90a3a234
Jeremy <aworetanjeremiah@gmail.com>
2024-09-26 17:49:00 +0100

    fix: update networks list

--- app/components/Waitlist.tsx
+++ app/components/Waitlist.tsx
@@ -1,25 +1,14 @@
-import Image from "next/image";
-import {
-  AnimatedComponent,
-  fadeInOut,
-  scaleInOut,
-  slideInOut,
-} from "./AnimatedComponents";
-import {
-  BNBIcon,
-  BaseIcon,
-  SolanaIcon,
-  TronIcon,
-  ZapIcon,
-} from "./ImageAssets";
-import { VideoDialog } from "./VideoDialog";
-import { WaitlistForm } from "./WaitlistForm";
+import Image from 'next/image';
+import { AnimatedComponent, fadeInOut, slideInOut } from './AnimatedComponents';
+import { BNBIcon, BaseIcon, PolygonIcon, ZapIcon } from './ImageAssets';
+import { VideoDialog } from './VideoDialog';
+import { WaitlistForm } from './WaitlistForm';
 
 const networks = [
-  { name: "BNB", Icon: BNBIcon },
-  { name: "Base", Icon: BaseIcon },
-  { name: "Tron", Icon: TronIcon },
-  { name: "Solana", Icon: SolanaIcon },
+  { name: 'Base', Icon: BaseIcon },
+  // { name: 'Arbitrum', Icon: ArbitrumIcon },
+  { name: 'Polygon', Icon: PolygonIcon },
+  { name: 'BNB', Icon: BNBIcon },
 ];
 
 const NetworkIcon = ({
@@ -30,7 +19,7 @@ const NetworkIcon = ({
   index: number;
 }) => (
   <Icon
-    className={`size-7 border-2 border-white bg-white transition dark:bg-neutral-900 dark:border-neutral-900 rounded-full ${index > 0 ? "-ml-2" : ""}`}
+    className={`size-7 border-2 border-white bg-white transition dark:bg-neutral-900 dark:border-neutral-900 rounded-full ${index > 0 ? '-ml-2' : ''}`}
   />
 );
 
@@ -46,20 +35,20 @@ export const Waitlist = ({
   return (
     <>
       <AnimatedComponent variant={slideInOut} delay={0.4}>
-        <h1 className="text-3xl font-semibold leading-normal text-neutral-900 dark:text-white">
-          So{" "}
-          <span className="text-primary font-extrabold font-playfair-display">
+        <h1 className='text-3xl font-semibold leading-normal text-neutral-900 dark:text-white'>
+          So{' '}
+          <span className='text-primary font-extrabold font-playfair-display'>
             fast
-          </span>{" "}
+          </span>{' '}
           they ask "How's that even possible?"
-          <ZapIcon className="size-6 inline-block align-middle" />
+          <ZapIcon className='size-6 inline-block align-middle' />
         </h1>
       </AnimatedComponent>
 
       <AnimatedComponent
         variant={fadeInOut}
         delay={0.6}
-        className="leading-normal text-neutral-900 dark:text-white/80 font-light"
+        className='leading-normal text-neutral-900 dark:text-white/80 font-light'
       >
         Convert your crypto to fiat at lightening speed. <br />
         Transfer them seamlessly to any bank account or mobile wallet.
@@ -72,10 +61,10 @@ export const Waitlist = ({
       <AnimatedComponent
         variant={fadeInOut}
         delay={1}
-        className="text-neutral-900 dark:text-white/80 font-light"
+        className='text-neutral-900 dark:text-white/80 font-light'
       >
         Supports
-        <div className="inline-flex align-middle mx-2">
+        <div className='inline-flex align-middle mx-2'>
           {networks.map(({ name, Icon }, index) => (
             <NetworkIcon key={name} Icon={Icon} index={index} />
           ))}
@@ -86,20 +75,20 @@ export const Waitlist = ({
       <AnimatedComponent variant={fadeInOut} delay={1.2}>
         <>
           <button
-            type="button"
+            type='button'
             onClick={() => setIsModalOpen(true)}
-            className="flex items-center gap-3.5 group"
+            className='flex items-center gap-3.5 group'
           >
             <Image
-              src="/video-icon.svg"
-              alt="Video Icon"
+              src='/video-icon.svg'
+              alt='Video Icon'
               width={24}
               height={24}
-              aria-label="Video Icon"
-              className="transition rounded-md w-6 h-5 inline-block align-middle border border-transparent group-hover:border-gray-300 group-hover:dark:border-white/20"
+              aria-label='Video Icon'
+              className='transition rounded-md w-6 h-5 inline-block align-middle border border-transparent group-hover:border-gray-300 group-hover:dark:border-white/20'
             />
 
-            <p className="text-neutral-900 dark:text-white/80 font-light group-hover:text-neutral-700 dark:group-hover:text-white transition-colors">
+            <p className='text-neutral-900 dark:text-white/80 font-light group-hover:text-neutral-700 dark:group-hover:text-white transition-colors'>
               See how it works
             </p>
           </button>
