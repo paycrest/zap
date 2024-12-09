48f188762c9e3600666915569dda5817f46898a1
Jeremy <aworetanjeremiah@gmail.com>
2024-12-09 22:15:02 +0100

    fix: update z-index and layout for disclaimer + adjust navbar positioning and layout
    Also adjust cookie consent button color on dark mode

--- app/components/Disclaimer.tsx
+++ app/components/Disclaimer.tsx
@@ -43,7 +43,7 @@ export const Disclaimer = () => {
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.3 }}
-          className="fixed inset-0 z-40 grid min-h-screen place-items-center gap-4 bg-black/25 backdrop-blur-sm"
+          className="fixed inset-0 z-[53] grid min-h-screen place-items-center gap-4 bg-black/50 backdrop-blur"
         >
           <motion.div
             initial={{ scale: 0.9, opacity: 0 }}
